d5534c41716810e903ce7634b8a6acc0e67c7b6a
mozhinag <manimozhinagooran06@gmail.com>
2024-01-24 12:19:40 +0530

    mentor

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const express = require(express);
+const express = require('express');
 const app = express();
+
+const port = 5001;
+const bodyparse = require('body-parser')
+const mentorRouter = require('./router/mentor')
+const studentRouter = require('./router/student')
+app.use(bodyparse.json())
+app.use('/mentor',mentorRouter)
+app.use('/student',studentRouter)
+app.get('/',(req,res)=>{
+res.json(`Hello`);
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running in ${port}`)});
